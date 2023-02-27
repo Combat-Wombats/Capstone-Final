d626b99d161b698e8fa54d2e01770a085009ba7e
Lex Trujillo <flyingskunkmonkey@gmail.com>
2023-02-27 15:37:49 +0000

    removed table creation and drops from seed data into index.js

--- server/db/index.js
+++ server/db/index.js
@@ -13,6 +13,11 @@ const {
 const syncTables = async()=> {
   console.log("syncing tables")
   const SQL = `
+    DROP TABLE IF EXISTS categories;
+    DROP TABLE IF EXISTS order_products;
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
 
   CREATE TABLE users(
     "userId" SERIAL PRIMARY KEY,
@@ -20,14 +25,39 @@ const syncTables = async()=> {
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255)
   );
+      CREATE TABLE products (
+    "productId" SERIAL PRIMARY KEY,
+    price INTEGER,
+    description text,
+    used BOOLEAN,
+    location text,
+    willDeliver BOOLEAN,
+    shipping BOOLEAN
+    );
+    CREATE TABLE categories (
+    id INTEGER REFERENCES products("productId"),
+    percussion_Drums INTEGER,
+    woodwinds INTEGER,
+    brass INTEGER,
+    accesories INTEGER,
+    test VARCHAR(100)
+    );
+    CREATE TABLE orders (
+    "orderId" SERIAL PRIMARY KEY,
+    "userId" INTEGER REFERENCES users("userId")
+    );
+    CREATE TABLE order_products(
+    "productId" INTEGER REFERENCES products("productId"),
+    "orderId" INTEGER REFERENCES orders("orderId")
+    )
+    
   `;
   await client.query(SQL);
 };
 
 const syncAndSeed = async()=> {
-  await dropTables();
   await syncTables();
-  await createTables();
+
   
 
 
--- server/db/seedData.js
+++ server/db/seedData.js
@@ -1,62 +1,10 @@
 const client = require("./client")
 
-async function createTables() {
-    await client.query(`
-    CREATE TABLE products (
-    "productId" SERIAL PRIMARY KEY,
-    price INTEGER,
-    description text,
-    used BOOLEAN,
-    location text,
-    willDeliver BOOLEAN,
-    shipping BOOLEAN
-    );
-    CREATE TABLE categories (
-    id INTEGER REFERENCES products("productId"),
-    percussion_Drums INTEGER,
-    woodwinds INTEGER,
-    brass INTEGER,
-    accesories INTEGER,
-    test VARCHAR(100)
-    );
-    CREATE TABLE orders (
-    "orderId" SERIAL PRIMARY KEY,
-    "userId" INTEGER REFERENCES users("userId")
-    );
-    CREATE TABLE order_products(
-    "productId" INTEGER REFERENCES products("productId"),
-    "orderId" INTEGER REFERENCES orders("orderId")
-    )
-    `)
-}
-
-async function dropTables() {
-    await client.query(`
-    DROP TABLE IF EXISTS categories;
-    DROP TABLE IF EXISTS order_products;
-    DROP TABLE IF EXISTS orders;
-    DROP TABLE IF EXISTS users;
-    DROP TABLE IF EXISTS products;
-    
-    `)
-}
 
 
-async function rebuildDB() {
-    try {
-        await dropTables()
-        await createTables()
-        
-       
-    } catch (error) {
-        console.log("error during rebuildDB")
-        throw error
-    }
-}
+
 
 module.exports = {
-    rebuildDB,
-    dropTables,
-    createTables,
+
     client
 }
