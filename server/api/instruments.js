4728833f3ed15b6faab1546e73ae50a529abbf57
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-03-01 12:32:53 -0500

    updating brass api

--- server/api/instruments.js
+++ server/api/instruments.js
@@ -2,6 +2,7 @@ const express = require ('express').Router();
 const db = require ('../db');
 const router = require('./auth');
 
+// GET /api/brass
 router.get('../instruments/brass', async(req, res, next)=>{
    try {
     const brass = await brass();
