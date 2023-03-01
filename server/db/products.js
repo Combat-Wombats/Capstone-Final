ce665b07c3c57a80bccacca47ab52fbe40f76788
Lex Trujillo <flyingskunkmonkey@gmail.com>
2023-03-01 21:12:17 +0000

    returning stringed instruments

--- server/db/products.js
+++ server/db/products.js
@@ -1,16 +1,16 @@
 const client = require('./client');
-////const jwt = require('jsonwebtoken');
-//const JWT = process.env.JWT;
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT;
 
-// const createProducts = async ({name, price, description, used, location, willDeliver, shipping})=>{
-// const SQL = `
-// INSERT INTO products(name, price, description, used, location, willDeliver, shipping)
-// VALUES ($1, $2, $3, $4, $5, $6, $7)
-// RETURNING *
-// `;
-// const response = await client.query(SQL, [name, price, description, used, location, willDeliver, shipping])
-// return response.rows[0]
-// }
+const createProducts = async ({price, name, description, used, location, willDeliver, shipping})=>{
+const SQL = `
+INSERT INTO products(price, name, description, used, location, willDeliver, shipping)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+RETURNING *
+`;
+const response = await client.query(SQL, [price, name, description, used, location, willDeliver, shipping])
+return response.rows[0]
+}
 
 module.exports = {
   createProducts
