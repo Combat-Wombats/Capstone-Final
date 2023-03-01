50913c696ff75e70450434897a6bf3bc2d8d8e43
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-03-01 10:26:42 -0600

    made changes to db instruments

--- server/db/instruments/woodwind.js
+++ server/db/instruments/woodwind.js
@@ -1,44 +1,52 @@
 const woodwind = () => {
     [
         {
-            Name: "Yamaha  YTS-62III Professional Tenor Saxophone",
-            Description: " One of the best-made tenors available, the YTS-62III features fully ribbed construction, yet its weight is comfortable and well balanced. It is hand-adjusted, and the two-piece bell, body and neck and are all hand-hammered to shape. Its new engraving design is likewise done by hand, instilling a uniqueness in each instrument. Multiple key posts are integrated into a single plate, which lends the horn moderate resistance while delivering a solid core with deep tone color. Hard steel springs offer a fast, sensitive key response. It has a Front F key along with a high F# key and an adjustable thumb rest.",
-            Features: [],
-            Price: "",
+            name: "Yamaha  YTS-62III Professional Tenor Saxophone",
+            description: " One of the best-made tenors available, the YTS-62III features fully ribbed construction, yet its weight is comfortable and well balanced. It is hand-adjusted, and the two-piece bell, body and neck and are all hand-hammered to shape. Its new engraving design is likewise done by hand, instilling a uniqueness in each instrument. Multiple key posts are integrated into a single plate, which lends the horn moderate resistance while delivering a solid core with deep tone color. Hard steel springs offer a fast, sensitive key response. It has a Front F key along with a high F# key and an adjustable thumb rest.",
+            features: [],
+            price: "",
+            location: "Phoenix",
             willDeliver: "yes",
-            Used: "no"
+            used: "no",
+            shipping: "yes"
 
         },
         {
-            Name: "  Etude EAS-100 Student Alto Saxophone  (Open Box)",
-            Description: "Made out of fine metals for superior tone production, the Etude EAS-100 is the perfect instrument for starting out on the alto saxophone. The keywork, pads and adjustment all work together to make tone production easier, more consistent and stronger. And the bell brace and construction help make it as durable as possible. The EAS-100 includes case, mouthpiece, cap and ligature. " + 
+            name: "  Etude EAS-100 Student Alto Saxophone  (Open Box)",
+            description: "Made out of fine metals for superior tone production, the Etude EAS-100 is the perfect instrument for starting out on the alto saxophone. The keywork, pads and adjustment all work together to make tone production easier, more consistent and stronger. And the bell brace and construction help make it as durable as possible. The EAS-100 includes case, mouthpiece, cap and ligature. " + 
                         " It may be tempting to buy a 'better' horn used, but saxophones are mechanically complex, and if the horn is not well regulated and set up properly, it will be extremely difficult for a student to play. It is perhaps advisable then for a new student to get a brand-new Etude EAS-100 as they are perfectly prepped to ready to play right out of the case. It will be shiny and beautiful, and your young saxophonist will jump up and down and squeal when he or she sees it. It's been proven that pride of ownership inspires practice, and practice leads to success. ",
-            Features: [],
-            Price: " $359.99",
+            features: [],
+            price: " $359.99",
+            location: "Honululu",
             willDeliver: "yes",
-            Used: 'yes'
+            used: 'yes',
+            shipping: "yes"
         },
         {
-            Name: "  Yamaha YCL-255 Standard Bb Clarinet Bb Clarinet",
-            Description: " The Yamaha YCL-255 is a newly redesigned Bb Clarinet with a matte ABS resin body, nickel-plated silver keys, 65mm barrel, and an adjustable thumb-rest with strap ring. It ships with the CL-4C mouthpiece.The YCL-255 combines state-of-the-art production expertise with high-end Yamaha clarinet features. The result is an outstanding entry-level instrument that delivers a warm, resonant tone that is remarkably similar to that of a wood clarinet. It features an ABS resin body with a matte finish that imitates the look of a wood clarinet and an adjustable thumb-rest with a neck strap ring that allows for even those with small hands to easily hold the instrument." + 
+            name: "  Yamaha YCL-255 Standard Bb Clarinet Bb Clarinet",
+            description: " The Yamaha YCL-255 is a newly redesigned Bb Clarinet with a matte ABS resin body, nickel-plated silver keys, 65mm barrel, and an adjustable thumb-rest with strap ring. It ships with the CL-4C mouthpiece.The YCL-255 combines state-of-the-art production expertise with high-end Yamaha clarinet features. The result is an outstanding entry-level instrument that delivers a warm, resonant tone that is remarkably similar to that of a wood clarinet. It features an ABS resin body with a matte finish that imitates the look of a wood clarinet and an adjustable thumb-rest with a neck strap ring that allows for even those with small hands to easily hold the instrument." + 
                          "  The new adjustable thumb-rest accommodates different sized hands, which helps the player develop proper playing technique. In addition, a strap ring allows for the instrument to be held without extra effort. Firmer screws and silver soldered ring gives this new feature excellent durability. The YCL-255's new bell made of solid and durable ABS resin results in a lighter instrument with improved response. ",
-            Features: [],
-            Price: "$1140.99",
+            features: [],
+            price: "$1140.99",
+            location: "Columbus",
             willDeliver: "yes",
-            Used: "No "
+            used: "No ",
+            shipping: "yes"
         },
         {
-            Name: "Etude Model EFL-100 Student Flute Closed Hole, Offset G, C Foot",
-            Description: " The Etude EFL-100 flute is a quality instrument to fit any budget. It features a silver-plated head, body and foot for beauty as well as durability. The C foot and offset-G key help make the instrument easier to hold and play for those with smaller haEtude believes that price should not be a barrier to experiencing the growth and joy that comes from making music. That's why Etude offers you five of the most popular woodwind and brass instruments at an incredible price: flute, clarinet, alto sax, trumpet and trombone. And no matter which you choose, you get a great design and outstanding construction that will give you years of musical enjoyment. See for yourself.  ",
-            Features: [],
-            Price: "",
+            name: "Etude Model EFL-100 Student Flute Closed Hole, Offset G, C Foot",
+            description: " The Etude EFL-100 flute is a quality instrument to fit any budget. It features a silver-plated head, body and foot for beauty as well as durability. The C foot and offset-G key help make the instrument easier to hold and play for those with smaller haEtude believes that price should not be a barrier to experiencing the growth and joy that comes from making music. That's why Etude offers you five of the most popular woodwind and brass instruments at an incredible price: flute, clarinet, alto sax, trumpet and trombone. And no matter which you choose, you get a great design and outstanding construction that will give you years of musical enjoyment. See for yourself.  ",
+            features: [],
+            price: "",
+            location: "Milwaukee",
             willDeliver: "yes",
-            Used: "no"
+            used: "no",
+            shipping: "yes"
         },
         {
-            Name: "Fox Renard Model 240 Bassoon",
-            Description: " ",
-            Features: [ "Red maple body",
+            name: "Fox Renard Model 240 Bassoon",
+            description: " ",
+            features: [ "Red maple body",
                         "Full German system plus high D and E keys",
                         "Short bore",
                         "Natural rubber linings in the wing joint and small bore of the boot joint",
@@ -53,19 +61,24 @@ const woodwind = () => {
                         "Flexible design may be used as high as A-443",
                         "Includes case, cover, and #2 and #3 CVX bore bocals"
                 ],
-            Price: "$11,565",
+            price: "$11,565",
+            location: "Detroit",
             willDeliver: "yes",
-            Used: "no"
+            used: "no",
+            shipping: "yes"
             
         },
         {
-            Name: "  Giardinelli GPC-300 Piccolo",
-            Description: " High-quality materials and attention to detail throughout make this easy-playing piccolo sound and look great. And it comes complete with an attractive wood case. " + 
+            name: "  Giardinelli GPC-300 Piccolo",
+            description: " High-quality materials and attention to detail throughout make this easy-playing piccolo sound and look great. And it comes complete with an attractive wood case. " + 
                         " The Giardinelli GPC-300 piccolo is a versatile instrument. Its silver-plated headjoint will give you the projection needed for outdoor playing, while the plastic body warms the tone for indoor use. This instrument, from the pads to the stainless steel screws to the hardshell case, is designed to maximize durability while still producing a high quality of sound. ",
-            Features: [],
-            Price: " $499.99",
+            features: [],
+            price: " $499.99",
+            location: "Tucson",
             willDeliver: "yes",
-            Used: "no"
+            used: "no",
+            shipping: "yes"
+
         }
 
     ]
