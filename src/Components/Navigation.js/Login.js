0c75dfa3bbfde03331876fbe995d15b0062bbcb4
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-03-17 00:35:28 -0500

    added css styling to Register and Login

--- src/Components/Navigation.js/Login.js
+++ src/Components/Navigation.js/Login.js
@@ -1,30 +1,32 @@
 import React, { useState } from 'react';
 
-const Login = ({ login })=> {
+const Login = ({ login }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const _login = (ev)=> {
+  const _login = (ev) => {
     ev.preventDefault();
     login({ username, password });
   };
   return (
-    <div className='login-section'>
-      <h1>Login</h1>
-      <form onSubmit={ _login }>
-        <input
-          placeholder='username'
-          value = { username }
-          onChange = { ev => setUsername(ev.target.value) }
+    <div className='login'>
+      <div className='loginComp'>
+        <h1>Login</h1>
+        <form onSubmit={_login}>
+          <input
+            placeholder='username'
+            value={username}
+            onChange={ev => setUsername(ev.target.value)}
           />
-        <input
-          placeholder='password'
-          type = 'password'
-          value={ password }
-          onChange = { ev => setPassword(ev.target.value) }
-        />
-        <button>Login</button>
-      </form>
+          <input
+            placeholder='password'
+            type='password'
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+          />
+          <button className="loginBtn" > LOGIN</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Components/Navigation.js/Register.js
+++ src/Components/Navigation.js/Register.js
@@ -2,47 +2,49 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchRegister } from '../../api';
 export const Register = (props) => {
-    const {setUser, setToken} = props;
+    const { setUser, setToken } = props;
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const register = async(ev) => {
+    const register = async (ev) => {
         ev.preventDefault();
         const register = await fetchRegister(username, password)
         const token = window.localStorage.getItem("token");
         console.log(register)
-        if(register.token){
+        if (register.token) {
             setToken(register.token);
             window.localStorage.setItem('token', token);
         }
-        if(register.user){
+        if (register.user) {
             setUser(register.user);
         }
     };
     return (
-        <div className='register-section'>
-             <h1>Register</h1>
-              <form onSubmit={ register }>
-                <input
-                placeholder='username'
-                value = { username }
-                onChange = { ev => setUsername(ev.target.value) }
-                />
-                <input
-                placeholder='password'
-                type = 'password'
-                value={ password }
-                onChange = { ev => setPassword(ev.target.value) }
-                />
-                <button>Register</button>
-            </form>   
-            <Link to='/Login'>
-                Already Have An Account? Click Here.
-            </Link>
+        <div className='register'>
+            <div className='registerComp'>
+                <h1>Register</h1>
+                <form onSubmit={register}>
+                    <input
+                        placeholder='username'
+                        value={username}
+                        onChange={ev => setUsername(ev.target.value)}
+                    />
+                    <input
+                        placeholder='password'
+                        type='password'
+                        value={password}
+                        onChange={ev => setPassword(ev.target.value)}
+                    />
+                    <button className ='registerBtn'>REGISTER</button>
+                </form>
+                <Link className="registerLink" to='/Login' >
+                    Already Have An Account? Click Here.
+                </Link>
+            </div>
         </div>
-       
+
 
     );
 };
- 
+
 export default Register
